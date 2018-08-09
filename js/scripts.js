aa7b9ea843a2a90b42a3f53ea145305f4456302c
Connor McCarthy and Meria Thomas <connor@>
2018-08-09 14:43:41 -0700

    change calculator to use radio buttions for operators

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,20 @@
-$(document).ready(function(){
-  $("form#triangles").submit(function(event){
-    var sideA = parseInt($("input#sideA").val());
-    var sideB = parseInt($("input#sideB").val());
-    var sideC = parseInt($("input#sideC").val());
-
-    var sum = sideA+sideB+sideC;
-    var max = Math.max(sideA,sideB,sideC);
-    var twoLength = sum-max;
-
-    if (twoLength > max) {
-      if (sideA === sideB && sideB=== sideC && sideA === sideC ){
-        $("#equilateral").show();
-        $("#scalene").hide();
-        $("#isosceles").hide();
-        $("#nope").hide();
-        //sideA === sideB || sideB=== sideC || sideC === sideA
-      } else if (sideA != sideB && sideB != sideC && sideA != sideC ){
-        $("#equilateral").hide();
-        $("#scalene").show();
-        $("#isosceles").hide();
-        $("#nope").hide();
-        //else
-      } else {
-        $("#equilateral").hide();
-        $("#scalene").hide();
-        $("#isosceles").show();
-        $("#nope").hide();
-      }
-    }
-      else{
-        $("#nope").show();
-        $("#equilateral").hide();
-        $("#scalene").hide();
-        $("#isosceles").hide();
-
-      }
-
-
-    // if ((sideA + sideB) > sideC || (sideB + sideC) >= sideA || (sideA + sideC) >= sideB){
-    //
-    // if (sideA === sideB && sideB=== sideC && sideA === sideC ){
-    //   $("#equilateral").show();
-    //   $("#scalene").hide();
-    //   $("#isosceles").hide();
-    //   $("#nope").hide();
-    //   //sideA === sideB || sideB=== sideC || sideC === sideA
-    // } else if (sideA != sideB && sideB != sideC && sideA != sideC ){
-    //   $("#equilateral").hide();
-    //   $("#scalene").show();
-    //   $("#isosceles").hide();
-    //   $("#nope").hide();
-    //   //else
-    // } else if (sideA === sideB || sideB === sideC || sideA === sideC){
-    //   $("#equilateral").hide();
-    //   $("#scalene").hide();
-    //   $("#isosceles").show();
-    //   $("#nope").hide();
-    // }
-    //
-    //
-    // }
-
-
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
     event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+
+    if (operator === "add") {
+      result = (number1 + number2);
+    } else if (operator === "subtract") {
+      result = (number1 - number2);
+    } else if (operator === "multiply") {
+      result = (number1 * number2);
+    } else if (operator === "divide") {
+      result = (number1 / number2);
+    }
+    $("#output").text(result);
   });
 });
